91dcaec3a25a44cc2fe7cf66e280262cdc7c48a1
Alexander Sanchez <alexanderwsanchez1991@gmail.com>
2021-05-12 10:54:36 -0500

    Add UI packages with npm.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
+import App from './components/app';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
