09a0eb8463c8f0e630145cbff4d69625bfc516f8
talk2dug <swayzej@Jacks-MacBook-Pro.local>
2021-05-25 20:17:51 -0500

    Video Streaming

--- src/components/nodeListStreamCameraModal.js
+++ src/components/nodeListStreamCameraModal.js
@@ -3,8 +3,8 @@ import CardGroup from 'react-bootstrap/CardGroup';
 import React, { useContext } from 'react';
 import { GlobalContext } from '../contexts/globalContext';
 import Modal from 'react-bootstrap/Modal';
-import { Player } from 'video-react';
-
+import JsmpegPlayer from "./jsmpegPlayer";
+import md5 from "md5"
 export default function NodeListStreamCameraModal() {
 
     const [state, dispatch] = useContext(GlobalContext);
@@ -14,28 +14,39 @@ export default function NodeListStreamCameraModal() {
       type: 'HOMESTREAMINGMODAL',
       payload: false,
     });
+    let checkSum1 = md5("/"+ state.currentNodeInfo.name +"/camera1-9999999999-nodemedia2017privatekey")
+    let rtmpURL1 = "https://crime-camera-system-restreamer.shreveport-it.org/"+ state.currentNodeInfo.name +"/camera1.flv?sign=9999999999-"+checkSum1
 
+    let checkSum2 = md5("/"+ state.currentNodeInfo.name +"/camera2-9999999999-nodemedia2017privatekey")
+    let rtmpURL2 = "https://crime-camera-system-restreamer.shreveport-it.org/"+ state.currentNodeInfo.name +"/camera2.flv?sign=9999999999-"+checkSum2
+
+    let checkSum3 = md5("/"+ state.currentNodeInfo.name +"/camera3-9999999999-nodemedia2017privatekey")
+    let rtmpURL3 = "https://crime-camera-system-restreamer.shreveport-it.org/"+ state.currentNodeInfo.name +"/camera3.flv?sign=9999999999-"+checkSum3
     return (
         <Modal show={state.homeStreamingModal} onHide={() => handleStreamModalClose()} centered size="lg">
+
         <Card className="text-center">
           <Card.Header as="h5">Streaming {state.homeSelectedNode}</Card.Header>
           <Card.Body>
             <CardGroup>
               <Card>
-                <Player autoPlay muted>
-                  <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
-                </Player>
+              <JsmpegPlayer
+                wrapperClassName="video-wrapper"
+                videoUrl={rtmpURL1}
+              />
               </Card>
               <Card>
-                <Player autoPlay muted>
-                  <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
-                </Player>
+              <JsmpegPlayer
+                wrapperClassName="video-wrapper"
+                videoUrl={rtmpURL2}
+              />              
               </Card>
               <Card>
-                <Player autoPlay muted>
-                  <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
-                </Player>
-              </Card>
+              <JsmpegPlayer
+                wrapperClassName="video-wrapper"
+                videoUrl={rtmpURL3}
+              />
+                      </Card>
             </CardGroup>
           </Card.Body>
         </Card>
