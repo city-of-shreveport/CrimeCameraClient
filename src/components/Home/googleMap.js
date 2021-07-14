8349d64f48153b33ac070f84e7cc177374796416
Alexander Sanchez <alexanderwsanchez1991@gmail.com>
2021-07-14 00:48:42 -0500

    Checkpoint.

--- src/components/Home/googleMap.js
+++ src/components/Home/googleMap.js
@@ -1,3 +1,4 @@
+import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button';
 import React, { useRef, useContext } from 'react';
 import { GlobalContext } from '../../contexts/globalContext';
@@ -11,57 +12,101 @@ var nodeStatus = false;
 export default function GoogleMap() {
   const [state, dispatch] = useContext(GlobalContext);
 
+
+
   const handleApiLoaded = (map, maps) => {
     const markers = [];
     const infowindows = [];
+    let nodeIcon = ''
+    var prev_infowindow =false; 
+    var current_infowindow = false;
+    const startStreaming =() => {
+    
+prev_infowindow.setContent("asdfasdfasdfasdf");
+          
 
+
+}
     nodes.map((node) => {
       var difference = getDifferenceInMinutes(new Date(node.lastCheckIn), new Date());
 
-      if (difference < 15) {
+      if (difference < 15) {nodeIcon = 'http://maps.google.com/mapfiles/kml/paddle/grn-blank.png'}
+        if (difference > 15) {nodeIcon = 'http://maps.google.com/mapfiles/kml/paddle/red-blank.png'}
         markers.push(
           new maps.Marker({
+            node: node.name,
             position: {
               lat: node.config.locationLat,
               lng: node.config.locationLong,
+              
             },
             map,
-            icon: 'http://maps.google.com/mapfiles/kml/paddle/grn-blank.png',
+            icon: {url: nodeIcon,
+            labelOrigin: { x: 33, y: 17}
+          },
             label: {
               text: node.name.substr(node.name.length - 3),
-              fontFamily: 'Material Icons',
+              
               color: 'black',
 
-              fontSize: '18px',
+              
             },
           })
         );
-        const infoWindoContent =
-          '<h4>' +
-          node.name +
-          '</h4' +
-          "<div className='card' style='width: 18rem;'>" +
-          "<img id='imgCamera1' width='200' height='150' src='http://10.10.30.10:3001/api/cameraConfig/snapshot/" +
-          node.name +
-          "/camera1' alt='Logo' />" +
-          "<img id='imgCamera2' width='200' height='150' src='http://10.10.30.10:3001/api/cameraConfig/snapshot/" +
-          node.name +
-          "/camera2' alt='Logo' />" +
-          "<img id='imgCamera3' width='200' height='150' src='http://10.10.30.10:3001/api/cameraConfig/snapshot/" +
-          node.name +
-          "/camera3' alt='Logo' />" +
-          '</div>';
+
+
+
+
         infowindows.push(
           new maps.InfoWindow({
-            content: infoWindoContent,
+            content: '',
           })
         );
-      }
+      
     });
 
     markers.forEach((marker, i) => {
+      function helloAlert(){
+alert('hello')
+
+}
+              const infoWindoContent = "<button id='helloAlert'>Start Streaming</button>"+
+                                "<div class='grid-container'>"+
+                                  "<div class='grid-item'>"+
+                                    "Camera 1"+
+                                  "</div>"+
+                                  "<div class='grid-item'>"+
+                                    "Camera 2"+
+                                  "</div>"+
+                                
+                                  "<div class='grid-item'>"+
+                                    "Camera 3"+
+                                  "</div>"+
+                                  "<div class='grid-item'>"+
+                                    "<img id='imgCamera1' width='200' height='150' src='http://10.10.30.10:3001/api/cameraConfig/snapshot/" + marker.node +
+                                      "/camera1' alt='Logo' />"+
+                                  "</div>"+
+                                  "<div class='grid-item'>"+
+                                    "<img id='imgCamera1' width='200' height='150' src='http://10.10.30.10:3001/api/cameraConfig/snapshot/" + marker.node +
+                                      "/camera2' alt='Logo' />"+
+                                  "</div>"+
+                                
+                                  "<div class='grid-item'>"+
+                                    "<img id='imgCamera1' width='200' height='150' src='http://10.10.30.10:3001/api/cameraConfig/snapshot/" + marker.node +
+                                      "/camera3' alt='Logo' />"+
+                                  "</div>"+
+                                
+                            "</div>"
       marker.addListener('click', () => {
+        if( prev_infowindow ) {
+           prev_infowindow.close();
+        }
+
+        prev_infowindow = infowindows[i];
+          infowindows[i].setContent(infoWindoContent);
+
         infowindows[i].open(map, marker);
+        current_infowindow = infowindows[i]
       });
     });
   };
@@ -108,9 +153,18 @@ export default function GoogleMap() {
       });
   };
   const AnyReactComponent = ({ text, status, node }) => (
-    <Button id={text} key={text} variant={status} onClick={() => handleOpenVideoStreamer({ node })}>
-      {text}
-    </Button>
+    <div>
+    <Card className="text-center">
+  <Card.Header>Featured</Card.Header>
+  <Card.Body>
+    <Card.Title>Special title treatment</Card.Title>
+    <Card.Text>
+      With supporting text below as a natural lead-in to additional content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+  <Card.Footer className="text-muted">2 days ago</Card.Footer>
+</Card></div>
   );
   const displayMarkers = () => {
     return state.nodes.map((node, index) => {
@@ -131,11 +185,13 @@ export default function GoogleMap() {
       {!isEmpty(state.nodes) && (
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyAxgBe1BLPLfPIPwK0ucb6-SeqkZdckChI' }}
-          defaultCenter={{ lat: 32.4368622, lng: -93.7550222 }}
-          defaultZoom={13}
+          defaultCenter={{ lat: 32.46, lng: -93.7550222 }}
+          defaultZoom={12}
           yesIWantToUseGoogleMapApiInternals
           onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
-        />
+        >
+       
+        </GoogleMapReact>
       )}
     </div>
   );
