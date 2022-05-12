0b70ab3b62fea6481f378d2864407dac7ebce88f
Jack Swayze <talk2dug@gmail.com>
2022-05-12 15:52:08 -0500

    Fixed updating and creating node

--- src/components/NodeManager/nodeManagerEditNodeModal.js
+++ src/components/NodeManager/nodeManagerEditNodeModal.js
@@ -26,8 +26,8 @@ export default function NodeManagerEditNodeModal() {
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formDataObject),
     };
-
-    fetch('http://10.10.200.10:3001/api/nodes/' + state.currentNodeInfo.name, requestOptions)
+    console.log(formDataObject)
+    fetch('http://127.0.0.1:3000/api/nodes/updateNode/' + state.currentNodeInfo.name, requestOptions)
       .then((response) => response.json())
       .then((data) => handleEditNodeModalClose());
   };
